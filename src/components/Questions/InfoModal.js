db26d38b07d420979a8aef1531ff9489fdb7dace
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-11 01:19:06 +0100

    add title attributes to delete and close buttons

--- src/components/Questions/InfoModal.js
+++ src/components/Questions/InfoModal.js
@@ -5,7 +5,7 @@ const InfoModal = (props) => {
     return (
         <div className="modal-wrapper" onClick={props.closeModal}>
             <div className="modal-content">
-                <div className="close-modal-button">&#10006;</div>
+                <div className="close-modal-button" title="Close">&#10006;</div>
                 <ul className="modal-info-list">
                     <li className="modal-info-label">Question</li>
                     <li className="modal-info-data">{props.question}</li>
