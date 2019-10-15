f1282a87db8fe85b394936065bdf1e40ded6acc2
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-16 00:00:29 +0100

    restrict url to 120 characters

--- src/components/UsefulLinks/LinkList.js
+++ src/components/UsefulLinks/LinkList.js
@@ -11,6 +11,9 @@ class LinkList extends React.Component {
         }
         let start = link.substr(0, index + 3)
         let iconUrl = start + '/favicon.ico'
+        if (link.length > 150) {
+            link = link.substr(0, 120) + '...'
+        }
         return <li className={styles.listItem}><img className={styles.favicon} src={iconUrl}></img><span className={styles.linkText}>{link}</span></li>
     }
 
