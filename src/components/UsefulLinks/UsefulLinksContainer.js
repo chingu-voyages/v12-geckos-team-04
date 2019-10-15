00d9eae8ec9ce8e71bff0b9a0002fbdc2d587644
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-15 22:55:57 +0100

    change to stateful class component

--- src/components/UsefulLinks/UsefulLinksContainer.js
+++ src/components/UsefulLinks/UsefulLinksContainer.js
@@ -1,17 +1,23 @@
 import React from 'react'
 
-const UsefulLinksContainer = () => {
+class UsefulLinksContainer extends React.Component {
 
-    const links = [
-        "https://en.wikipedia.org/wiki/Brigid_Kosgei",
-        "https://css-tricks.com/favicons-next-to-external-links/",
-        "https://stackoverflow.com/questions/52324664/how-to-use-iframe-in-react-js",
-        "https://medium.com/@jamesfuthey/simulating-the-creation-of-website-thumbnail-screenshots-using-iframes-7145269891db",
-        "https://www.w3schools.com/html/html_iframe.asp",
-        "https://www.google.com/search?q=regex+everything+after+something&rlz=1C1CHBF_enGB862GB862&oq=regex+everything+after+something&aqs=chrome..69i57.3196j1j4&sourceid=chrome&ie=UTF-8"
-    ]
+    state = {
+        links: [
+            "https://en.wikipedia.org/wiki/Brigid_Kosgei",
+            "https://css-tricks.com/favicons-next-to-external-links/",
+            "https://stackoverflow.com/questions/52324664/how-to-use-iframe-in-react-js",
+            "https://medium.com/@jamesfuthey/simulating-the-creation-of-website-thumbnail-screenshots-using-iframes-7145269891db",
+            "https://www.w3schools.com/html/html_iframe.asp",
+            "https://www.google.com/search?q=regex+everything+after+something&rlz=1C1CHBF_enGB862GB862&oq=regex+everything+after+something&aqs=chrome..69i57.3196j1j4&sourceid=chrome&ie=UTF-8"
+        ]
+    }
 
-    const getFavicon = (link) => {
+    componentDidMount() {
+
+    }
+
+    getFavicons = (link) => {
         let index
         index = link.indexOf('com')
         if (index === -1) {
@@ -22,13 +28,14 @@ const UsefulLinksContainer = () => {
         return <img src={iconUrl} width="20px" height="20px"></img>
     }
 
+    render() {
 
-    return (
-        <div>
-            {links.map(getFavicon)}
-        </div>
-    )
+        return (
+            <div>
+                {this.state.links.map(this.getFavicons)}
+            </div>
+        )
+    }
 }
 
 export default UsefulLinksContainer
-
