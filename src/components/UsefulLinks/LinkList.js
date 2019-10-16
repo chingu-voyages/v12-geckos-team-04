86da9ca0241f6608bea27e927a5b9d3313014a0b
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-17 00:02:37 +0100

    put links in anchor tags and add href to open in new tab

--- src/components/UsefulLinks/LinkList.js
+++ src/components/UsefulLinks/LinkList.js
@@ -5,21 +5,21 @@ import defaultIcon from './images/default.ico'
 class LinkList extends React.Component {
 
     getUrlAndIcon = (link, index) => {
+        let href
+        if (link.substr(0, 4) !== 'http') {
+            href = 'http://' + link
+        }
         let domainIndex
         domainIndex = link.indexOf('com')
         if (domainIndex === -1) {
             domainIndex = link.indexOf('org')
         }
         let start = link.substr(0, domainIndex + 3)
-        let iconUrl = start + '/favicon.ico'
-        if (iconUrl.substr(0, 4) !== 'http') {
-            const protocol = 'http://'
-            iconUrl = protocol + iconUrl
-        }
+        let iconUrl = href + '/favicon.ico'
         if (link.length > 150) {
             link = link.substr(0, 120) + '...'
         }
-        return <li key={index} className={styles.listItem}><img onError={this.handleError} alt={'Icon for website'} className={styles.favicon} src={iconUrl}></img><span className={styles.linkText}>{link}</span><span onClick={(e) => {this.props.deleteLink(e, index)}} title="Delete this link" className={styles.closeButton}>&times;</span></li>
+        return <li key={index} className={styles.listItem}><img onError={this.handleError} alt={'Icon for website'} className={styles.favicon} src={iconUrl}></img><a href={href} target='_blank' className={styles.linkText}>{link}</a><span onClick={(e) => {this.props.deleteLink(e, index)}} title="Delete this link" className={styles.closeButton}>&times;</span></li>
     }
 
     handleError = (e) => {
