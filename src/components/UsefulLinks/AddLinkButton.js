d28b8747d4b3d032d8caff9ebdc16c8d2f17c835
Thomas McAndrew <tommcandrew@hotmail.com>
2019-10-17 11:14:11 +0100

    style button

--- src/components/UsefulLinks/AddLinkButton.js
+++ src/components/UsefulLinks/AddLinkButton.js
@@ -4,8 +4,8 @@ import styles from './AddLinkButton.module.scss'
 const AddLinkButton = (props) => {
     return (
         <form className={styles.form} onSubmit={(e) => {props.addLink(e)}}>
-            <button type="submit">Add New Link</button>
-            <input type="text" name="textInput" />
+            <button type="submit" className={styles.button}>Add New Link</button>
+            <input type="text" name="textInput" className={styles.input} />
         </form>
     )
 }
